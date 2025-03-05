8724b24d415e3117fd0b53d5aa3418b80fa4197b
Lion Halika <lionhalika@gmail.com>
2025-03-05 07:41:57 +0200

    something else

--- agents_0/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import importlib
-
-modules = [f[:-3] for f in os.listdir(os.path.dirname(__file__)) if f.endswith(".py") and f != "__init__.py"]
-for module in modules:
-    importlib.import_module(f"agents.{module}")
